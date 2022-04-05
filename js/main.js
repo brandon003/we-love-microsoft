6730d0dce84a3f7916d8c72c9fe0526450487cf3
Brandon Lobo <brandon_slobo@hotmail.com>
2022-04-05 10:46:56 -0500

    more changes

--- js/main.js
+++ js/main.js
@@ -20,31 +20,32 @@ let arr = [1,2,3,4,5,6,7,8,9];
 function eachCons(array, n) {
     let cascadeList = [];
     cascadeList = array.map(function (x, i, v,) { cascadeArray(x, i, v, n) });
-    for(let w = 0;w<cascadeList.length; w++){
-    console.log(cascadeList[w]);
-    }
-    return cascadeList;
+
+    
+    console.log(cascadeList);
+    console.log(cascadeList.length)
+    // return cascadeList;
 }
 
 function cascadeArray(x, i, v, n) {
-    let array=[];
-    console.log(x,i,v,n);
+    let array;
+    console.log(`For Each x=${x}, i=${i}, v=${v}, n=${n}`);
     if((n > 0) && ((i+n)<=v.length)){
         for (let j = 0; j+i < i+n; j++) {
             array.push(v[i+j]);
-            // console.log(n, j, i, j+i,i+n, x, v[i + j])
-            console.log(array);
+            console.log(`For Loop n=${n}, j=${j}, i=${i}, j+i=${j+i} < i+n=${i+n} , x=${x} , v[i + j]=${v[i + j]}`)
+            console.log('loop array ',array);
         }
         console.log[array];    
         
-    } else array = x;
+    } else array=x;
 
-    console.log[array];
+    // console.log[array];
     return array;
 }
 
 
-eachCons(arr,1);
+eachCons(arr,0);
 // console.log(eachCons(arr,3));
 
 function check(arr, x) {
